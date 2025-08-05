e6b388dbec139ac1236f59d73d4132eba3984c25
sureshsk9 <suresh@settleking.com>
2025-08-05 19:07:32 +0530

    Inheritance P-4

---
+++ Main1.java
@@ -0,0 +1,13 @@
+ class Main1 {
+    int age;
+    Main1(int age){
+        //age =age;
+       this.age=age;
+
+    }
+    public static void main(String[] args){
+        Main1 obj = new Main1(24);
+        System.out.println("obj.age="+obj.age);
+    }
+    
+}
